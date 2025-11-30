97c0d643d910d53ba2557062e90a67f848dfa2c3
Tejas Sonawane <sonawaneteju4@gmail.com>
2025-12-01 01:09:28 +0530

    feat: Refactor Razorpay order creation logic to validate payment method before checking pricing option

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -413,16 +413,7 @@ const createBooking = asyncHandler(async (req, res) => {
     );
     logger.info(`[BOOKING] Booking created: ${booking[0]?._id}`);
 
-    if (paymentMethod === 'online') {
-      if (!option || typeof option.price !== 'number') {
-        logger.error(
-          `[BOOKING] Cannot create Razorpay order: option or price missing`
-        );
-        throw new ApiError(
-          400,
-          'Pricing option or price missing for online payment'
-        );
-      }
+    if (paymentMethod !== 'online') {
       order = await razorpay.orders.create({
         amount: option.price * 100,
         currency: 'INR',
